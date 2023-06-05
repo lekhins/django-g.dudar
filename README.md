7aadcf124403e670b46ccb77fce629773db08800
REPO <a.alekseenko@yandex.ru>
2023-06-05 06:26:10 +0300

    закончил 4-й блок

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# django-g.dudar
+ДАТА: 05.06.2023 в 06:24
+Край: https://www.youtube.com/watch?v=OYeqcxaYUbQ&list=PLDyJYA6aTY1nZ9fSGcsK4wqeu-xaJksQQ&index=5
+Продолжение: начать новую
--- itproger/main/views.py
+++ itproger/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def contacts(request):
+    return HttpResponse('<h4>8 (800) 555-35-35 </h4>')
